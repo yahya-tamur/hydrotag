775cad4422d9e0b1b150208e301c35642d41e502
kerem <77764573+yahya-tamur@users.noreply.github.com>
2023-07-07 13:03:43 -0700

    refactored index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import mapstyles from '../styles/Map.module.css';
 import { useState, useEffect } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import { initializeApp } from "firebase/app";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore, collection, getDocs, getDoc } from 'firebase/firestore';
 import { useRouter } from 'next/router';
 
@@ -10,25 +10,10 @@ import firebaseConfig from '../firebaseConfig';
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app);
-const user = auth.currentUser
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/auth.user
-    const uid = user.uid;
-    console.log(user.uid + 'is logged in')
-    // ...
-  } else {
-    console.log('Guest user')
-    const uid = "Guest"
-    // User is signed out
-    // ...
-  }
-});
 
 export default function Home() {
+
+  const router = useRouter();
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
@@ -38,17 +23,25 @@ export default function Home() {
 
   return (
     <div style={{ height: `100%`, width: `100%` }} >
+      <h1>
+        <div className="banner">
+          <div className="banner-content">
+            <h2>{auth.currentUser?.uid ?? "Guest"}</h2>
+            <button type="button" onClick={() => { router.push("posts/login") }}> click here to login </button>
+            <p>Hydrotag</p>
+          </div>
+        </div>
+      </h1>
       <Map />
     </div>
   );
 }
 
 function Map() {
-  const router = useRouter();
 
-  //learn about react, useState.
+  const db = getFirestore(app);
+
   let [markerlist, setMarkerList] = useState([]);
-
 
   //this is how you have to fetch data from the database.
   useEffect(() => {
@@ -93,22 +86,10 @@ function Map() {
     null,
     new window.google.maps.Size(40, 40)
   );
-  var userBanner = 'Guest'
-  if (user?.uid != null) {
-    userBanner = user.uid
-  }
 
   return (
     <div>
-      <h1>
-        <div className="banner">
-          <div className="banner-content">
-            <h2>{userBanner}</h2>
-            <button type="button" onClick={() => { router.push("posts/login") }}> click here to login </button>
-            <p>Hydrotag</p>
-          </div>
-        </div>
-      </h1>
+
       <GoogleMap
         zoom={10}
         center={{ lat: 44, lng: -80 }}
