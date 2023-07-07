086813474a083a6ea76d79de095e689b48a750a8
kerem <77764573+yahya-tamur@users.noreply.github.com>
2023-07-07 12:42:14 -0700

    frontend now fetches data from firestore

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import { useState, useEffect } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { useRouter} from 'next/router';
+import { getFirestore, collection, getDocs, getDoc } from 'firebase/firestore';
+import { useRouter } from 'next/router';
 
 import firebaseConfig from '../firebaseConfig';
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 const user = auth.currentUser
 
 onAuthStateChanged(auth, (user) => {
@@ -35,7 +37,7 @@ export default function Home() {
   if (!isLoaded) return <div>Loading...</div>;
 
   return (
-    <div style={{height: `100%`, width: `100%`} } >
+    <div style={{ height: `100%`, width: `100%` }} >
       <Map />
     </div>
   );
@@ -47,27 +49,50 @@ function Map() {
   //learn about react, useState.
   let [markerlist, setMarkerList] = useState([]);
 
-  /*
-  //this is how you have to fetch data from the backend.
+
+  //this is how you have to fetch data from the database.
   useEffect(() => {
     const dataFetch = async () => {
-      const request = await fetch (
-          "http://localhost:3001/markers",
-          {mode: "cors"}
-        );
-      setMarkerList(await request.json());
+
+      //get data from firestore.
+      const snapshot = await getDocs(collection(db, 'markers'));
+
+      //transforms the data we receive into a convenient form.
+      let arr = [];
+      snapshot.forEach((doc) => {
+        console.log(doc.data());
+        arr.push({
+          id: doc.id,
+          name: doc.data().name,
+          lat: doc.data().location._lat,
+          lng: doc.data().location._long,
+          poster: doc.data().poster,
+        });
+
+      });
+
+      //for the 'poster' fields, since they're references to users, we
+      //make more requests to firestore to get the data about the users they
+      //refer to.
+      for (const marker of arr) {
+        marker.poster = await getDoc(marker.poster).then(res => res.data());
+      }
+
+      setMarkerList(arr);
+
     };
+
     dataFetch();
   }, []);
-*/
+
 
   let iconMarker = new window.google.maps.MarkerImage(
     "/water-fountain.jpg",
     null,
     null,
-    null, 
+    null,
     new window.google.maps.Size(40, 40)
-);
+  );
   var userBanner = 'Guest'
   if (user?.uid != null) {
     userBanner = user.uid
@@ -79,46 +104,46 @@ function Map() {
         <div className="banner">
           <div className="banner-content">
             <h2>{userBanner}</h2>
-            <button type="button" onClick={() => {router.push("posts/login")}}> click here to login </button>
+            <button type="button" onClick={() => { router.push("posts/login") }}> click here to login </button>
             <p>Hydrotag</p>
           </div>
         </div>
       </h1>
       <GoogleMap
-      zoom={10}
-      center={{ lat: 44, lng: -80 }}
-      mapContainerClassName={mapstyles.mapcontainer}
-      onClick={e => {
+        zoom={10}
+        center={{ lat: 44, lng: -80 }}
+        mapContainerClassName={mapstyles.mapcontainer}
+        onClick={e => {
 
-        // adds a new marker to markerlist. you have to clone the array,
-        // add something to the cloned array, then call setMarkerList in 
-        // order for the page to update properly.
-        const ml = [...markerlist]
-        ml.push({
-          id: "placeholder",
-          name: "new water fountain",
-          lat: e.latLng.lat(),
-          lng: e.latLng.lng(),
-          poster: {username:"test1"}
-        })
-        setMarkerList(ml);
+          // adds a new marker to markerlist. you have to clone the array,
+          // add something to the cloned array, then call setMarkerList in 
+          // order for the page to update properly.
+          const ml = [...markerlist]
+          ml.push({
+            id: "placeholder",
+            name: "new water fountain",
+            lat: e.latLng.lat(),
+            lng: e.latLng.lng(),
+            poster: { username: "test1" }
+          })
+          setMarkerList(ml);
 
         }
-      }>
+        }>
 
-      {
-        markerlist.map((marker, i) =>
-          <Marker
-            icon={iconMarker}
-            position={marker}
-            key={i}
-            onClick={e => {
-              alert(`clicked ${marker.name}`);
-            }}
-          />
-        )
-      } 
+        {
+          markerlist.map((marker, i) =>
+            <Marker
+              icon={iconMarker}
+              position={marker}
+              key={i}
+              onClick={e => {
+                alert(`clicked ${marker.name}`);
+              }}
+            />
+          )
+        }
       </GoogleMap>
     </div>
   );
- }
+}
