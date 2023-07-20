8c8c5337a970a8eddbfe6009968590c47bb92f5e
Evelyn Johnson <evijohns@ucsc.edu>
2023-07-19 21:50:10 -0700

    BADGES COMPLETE

--- components/Profile.js
+++ components/Profile.js
@@ -17,6 +17,16 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import DirectionsBoatFilledSharpIcon from '@mui/icons-material/DirectionsBoatFilledSharp';
+import Diversity1SharpIcon from '@mui/icons-material/Diversity1Sharp';
+import WaterDropSharpIcon from '@mui/icons-material/WaterDropSharp';
+import VerifiedSharpIcon from '@mui/icons-material/VerifiedSharp';
+import RateReviewSharpIcon from '@mui/icons-material/RateReviewSharp';
+import EmojiEventsSharpIcon from '@mui/icons-material/EmojiEventsSharp';
+import Tooltip from '@mui/material/Tooltip';
 import { Box } from '@mui/system';
 import { styled } from '@mui/system';
 
@@ -45,6 +55,11 @@ export default function Profile() {
     const [openProfile, setOpenProfile] = useState(false);
     const [profileUserId, setProfileUserId] = useState(null);
     const [searchText, setSearchText] = useState("");
+    const [openReport, setOpenReport] = useState(false);
+    const [reportType, setReportType] = useState("");
+    const [reportText, setReportText] = useState("");
+    const [userIdToReport, setUserIdToReport] = useState(null);
+    const reportTypes = ["Falsely pinning a water source", "Inappropriate reviews", "Spamming", "Being a bully"];
     const router = useRouter();
 
     useEffect(() => {
@@ -103,6 +118,34 @@ export default function Profile() {
         setOpenProfile(true);
     };
 
+    const handleCloseReport = () => {
+        setOpenReport(false);
+    };
+
+    const handleOpenReport = (userId) => {
+        setUserIdToReport(userId);
+        setOpenReport(true);
+    };
+
+    const handleSubmitReport = async () => {
+        if (reportType && reportText) {
+            const time = serverTimestamp();
+            await addDoc(collection(db, "reports"), {
+                reporter: auth.currentUser.uid,
+                reportedUser: userIdToReport,
+                timestamp: time,
+                type: reportType,
+                description: reportText
+            });
+            setReportType("");
+            setReportText("");
+            handleCloseReport();
+            alert("Your report has been submitted and is being reviewed.");
+        } else {
+            alert("Please fill in all fields to submit a report.");
+        }
+    };
+
     const filteredFollowings = searchText
         ? followings.filter((following) =>
             users.find(user => user.id === following.following)?.email.toLowerCase().includes(searchText.toLowerCase())
@@ -114,6 +157,12 @@ export default function Profile() {
             users.find(user => user.id === followerId)?.email.toLowerCase().includes(searchText.toLowerCase())
         )
         : followers;
+    const handleReport = (userId) => {
+        handleOpenReport(userId);
+    };
+    const handleOpenBadgeDetails = (badgeName) => {
+        console.log(`Clicked on ${badgeName}`);
+    };
 
     return (
         <Box sx={{ display: 'flex', flexDirection: 'row' }}>
@@ -146,8 +195,88 @@ export default function Profile() {
 
             </Box>
             <Box sx={{ flex: '1 1', p: 2 }}>
-                <Typography variant="h5">Will be adding user badges and water streak here !!!</Typography>
+                <Typography variant="h5">Badges</Typography>
+                <Box display="flex" flexDirection="column" alignItems="center" marginTop="2em">
+                    {/* First Row */}
+                    <Box display="flex" justifyContent="center" alignItems="center">
+                        {/* Badge 1 */}
+                        <Tooltip title="Awarded for joining the HydroTag community">
+                            <Box display="flex" flexDirection="column" alignItems="center" marginX="2em" onClick={() => handleOpenBadgeDetails("Welcome Aboard")} style={{ cursor: "pointer" }}>
+                                <DirectionsBoatFilledSharpIcon sx={{ fontSize: '3rem', color: 'gold' }} />
+                                <Typography variant="h6">Welcome Aboard</Typography>
+                            </Box>
+                        </Tooltip>
+                        {/* Badge 2 */}
+                        <Tooltip title="Given for adding a new friend on HydroTag">
+                            <Box display="flex" flexDirection="column" alignItems="center" marginX="2em" onClick={() => handleOpenBadgeDetails("Social Hydrator")} style={{ cursor: "pointer" }}>
+                                <Diversity1SharpIcon sx={{ fontSize: '3rem', color: 'gold' }} />
+                                <Typography variant="h6">Social Hydrator</Typography>
+                            </Box>
+                        </Tooltip>
+                        {/* Badge 3 */}
+                        < Tooltip title="Given for pinning their first water marker" >
+                            <Box display="flex" flexDirection="column" alignItems="center" marginX="2em" onClick={() => handleOpenBadgeDetails("Water Tracker Pioneer")} style={{ cursor: "pointer" }}>
+                                <WaterDropSharpIcon sx={{ fontSize: '3rem', color: 'gold' }} />
+                                <Typography variant="h6">Water Tracker Pioneer</Typography>
+                            </Box>
+                        </Tooltip >
+                    </Box>
+                    {/* Second Row */}
+                    <Box display="flex" justifyContent="center" alignItems="center" marginTop="2em" onClick={() => handleOpenBadgeDetails("Hydro Influencer")} style={{ cursor: "pointer" }}>
+                        {/* Badge 4 */}
+                        < Tooltip title="Awarded for having five followers on HydroTag" >
+                            <Box display="flex" flexDirection="column" alignItems="center" marginX="2em">
+                                <VerifiedSharpIcon sx={{ fontSize: '3rem', color: 'gold' }} />
+                                <Typography variant="h6">Hydro Influencer</Typography>
+                            </Box>
+                        </Tooltip >
+                        {/* Badge 5 */}
+                        < Tooltip title="Given for writing their first review on HydroTag" >
+                            <Box display="flex" flexDirection="column" alignItems="center" marginX="2em" onClick={() => handleOpenBadgeDetails("Hydro Critic")} style={{ cursor: "pointer" }}>
+                                <RateReviewSharpIcon sx={{ fontSize: '3rem', color: 'gold' }} />
+                                <Typography variant="h6">Hydro Critic</Typography>
+                            </Box>
+                        </Tooltip >
+                        {/* Badge 6 */}
+                        <Tooltip title="Awarded for being number 1 on the HydroTag leaderboard">
+                            <Box display="flex" flexDirection="column" alignItems="center" marginX="2em" onClick={() => handleOpenBadgeDetails("Aquatic Ace")} style={{ cursor: "pointer" }}>
+                                <EmojiEventsSharpIcon sx={{ fontSize: '3rem', color: 'gold' }} />
+                                <Typography variant="h6">Aquatic Ace</Typography>
+                            </Box>
+                        </Tooltip>
+                    </Box>
+                </Box>
             </Box>
+
+            {/* Report User Dialog */}
+            <Dialog open={openReport} onClose={handleCloseReport}>
+                <DialogTitle>Report User</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Please fill in the following details for your report.
+                    </DialogContentText>
+                    <FormControl fullWidth>
+                        <InputLabel>Report Type</InputLabel>
+                        <Select value={reportType} onChange={(e) => setReportType(e.target.value)}>
+                            {reportTypes.map((type, index) => (
+                                <MenuItem value={type} key={index}>{type}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                    <TextField
+                        fullWidth
+                        margin="dense"
+                        label="Report Description"
+                        type="text"
+                        value={reportText}
+                        onChange={(e) => setReportText(e.target.value)}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseReport}>Cancel</Button>
+                    <Button onClick={handleSubmitReport}>Submit Report</Button>
+                </DialogActions>
+            </Dialog>
 
             {/* The rest of your Dialogs and Dialog components here */}
             <Dialog onClose={() => setOpenFollowers(false)} open={openFollowers}>
@@ -178,6 +307,7 @@ export default function Profile() {
                         <ListItem key={followerId}>
                             <Typography>{users.find(user => user.id === followerId)?.email}</Typography>
                             {followings.find(following => following.following === followerId) && <StarIcon />}
+                            <Button onClick={() => handleReport(followerId)} style={{ color: '#209cee' }}>Report</Button>
                             <Button onClick={() => handleOpenProfile(followerId)} style={{ color: '#209cee' }}>Profile</Button>
                         </ListItem>
                     ))}
@@ -212,8 +342,9 @@ export default function Profile() {
                         <ListItem key={following.id}>
                             <Typography>{users.find(user => user.id === following.following)?.email}</Typography>
                             {followers.includes(following.following) && <StarIcon />}
-                            <Button onClick={() => handleOpenProfile(following.following)} style={{ color: '#209cee' }}>Profile</Button>
                             <Button onClick={() => handleUnfollow(following.id)} style={{ color: '#209cee' }}>Unfollow</Button>
+                            <Button onClick={() => handleReport(following.following)} style={{ color: '#209cee' }}>Report</Button>
+                            <Button onClick={() => handleOpenProfile(following.following)} style={{ color: '#209cee' }}>Profile</Button>
                         </ListItem>
                     ))}
                 </List>
@@ -222,6 +353,14 @@ export default function Profile() {
         </Box >
     );
 }
+
+
+
+
+
+
+
+
 
 
 
